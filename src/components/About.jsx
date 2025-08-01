f4695bd1373faf943a404aeaac362d028b6d1637
Krishna41357 <154420450+Krishna41357@users.noreply.github.com>
2025-08-01 05:36:09 +0530

    eigth

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ import { useEffect, useRef } from 'react';
      
 
       {/* Background Elements */}
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 overflow-x-hidden">
         <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-purple-500/3 rounded-full blur-3xl"></div>
         <div className="absolute bottom-1/4 right-1/4 w-80 h-80 bg-blue-500/3 rounded-full blur-3xl"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-72 h-72 bg-purple-400/2 rounded-full blur-3xl"></div>
